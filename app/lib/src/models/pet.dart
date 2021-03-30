c81f6ea064dcb5f2eaa2653bec9de8c2316bf39d
Juan Pablo Cano <jp.cano@uniandes.edu.co>
2021-03-30 18:44:56 -0500

    Pet Model

---
+++ app/lib/src/models/pet.dart
@@ -0,0 +1,15 @@
+class Pet {
+  String name;
+  String type;
+  String breed;
+  String height;
+  String weight;
+  int age;
+  String birthday;
+
+  Map<String, double> location;
+
+  Pet(this.name, this.age);
+
+  
+}
